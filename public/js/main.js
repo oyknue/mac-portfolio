b7bc9d48b860bb5b1e0ea0772bf004e0ef308219
oyknue <oyknue.lee@gmail.com>
2019-06-19 10:08:44 +0900

    js window open

--- public/js/main.js
+++ public/js/main.js
@@ -63,22 +63,22 @@ $(".doc li").each(function(){
     window.open("https://github.com/oyknue", "_blank", "toolbar=no, top=25,left=50, width=500px, height=800px");
   }); //깃
   $("#memo").click(function(e){
-    window.open("https://oyknue-note.firebaseapp.com/html/note.html", "_blank", "toolbar=no, top=25,left=50, width=500, height=800");
+    window.open("https://oyknue-note.firebaseapp.com/html/note.html", "_blank", "toolbar=no, top=25,left=50, width=800, height=1000");
   }); //메모앱
   $("#weather").click(function(){
-    window.open("https://oyknue-weather.firebaseapp.com", "_blank", "toolbar=no, top=25,left=50,width=500, height=800");
+    window.open("https://oyknue-weather.firebaseapp.com", "_blank", "toolbar=no, top=25,left=50,width=1000, height=800");
   }); //날씨
   $("#game").click(function(){
-    window.open("https://racing-oyknue.web.app", "_blank", "toolbar=yes, top=25,left=50,width=1200, height=800");
+    window.open("https://racing-oyknue.web.app", "_blank", "toolbar=yes, top=25,left=50,width=1800, height=1000");
   }); //경마게임
   $("#siri").click(function(){
     window.open("https://racing-oyknue.web.app", "_blank", "toolbar=no, top=25,left=50,width=500, height=800");
   }); //프로필
   $("#safari").click(function(){
-    window.open("https://oyknue-pulmuone.web.app", "_blank", "toolbar=no, top=25,left=50,width=500, height=800");
+    window.open("https://oyknue-pulmuone.web.app", "_blank", "toolbar=no, top=25,left=50,width=1800, height=1000");
   }); //풀무원
   $("#chrome").click(function(){
-    window.open("https://oyknue-pr.web.app", "_blank", "toolbar=no, top=25,left=50, width=500, height=800");
+    window.open("https://oyknue-pr.web.app", "_blank", "toolbar=no, top=25,left=50, width=1800, height=1000");
   }); // 르쿠르제
   $("#score").click(function(){
     window.open("http://oyknue.dothome.co.kr/score/index.html", "_blank", "toolbar=no, top=25,left=50,width=500, height=800");
@@ -97,6 +97,7 @@ $(function() {
   });
   $( ".folder, .fodal" ).droppable();
 });
+
 //folder name
 $(".folder").each(function(){
   $(this).hover(function(){
