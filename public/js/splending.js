1561fc46dfe4c9daf041cf533aeab236d15aab28
oyknue <oyknue.lee@gmail.com>
2019-07-01 12:02:04 +0900

    splending 반응형

--- public/js/splending.js
+++ public/js/splending.js
@@ -1,10 +1,17 @@
 var log = console.log;
 var a = alert;
 
-//splending후 로그인 페이지 이동
-$(document).ready(function(){
+// splending후 로그인 페이지 이동
+var windowWidth = $( window ).width();
+if(windowWidth < 1440){
     $(".loading_bar").animate({
-        "width" : "45rem"
+        "width" : "40rem"
     }, 5000);
-    setTimeout("location.href='../html/login.html'",5000);
-});
+}
+else{
+   $(".loading_bar").animate({
+        "width" : "45rem"
+    }, 5000); 
+}
+
+setTimeout("location.href='../html/login.html'",5000);
